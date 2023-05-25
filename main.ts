9bb9e4ba6655c874cd06eaa9d30e956fb4be0f76
Daniel Wagner <daniel@wagners.name>
2023-05-25 22:40:36 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber >= Würfel) {
+    if (receivedNumber >= dice) {
         basic.showIcon(IconNames.Sad)
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    Würfel = randint(1, 6)
-    basic.showNumber(Würfel)
-    radio.sendNumber(Würfel)
+    dice = randint(1, 6)
+    basic.showNumber(dice)
+    radio.sendNumber(dice)
 })
-let Würfel = 0
+let dice = 0
 radio.setGroup(1)
 basic.forever(function () {
 	
